4da5089624abc42020394637affb97954d4f9510
Ahn Jungho <ahnjungho@ahnjungho.org>
2015-07-08 21:01:44 +0900

    add fields to document admin

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,7 @@ from pages.models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
+    fields = ['title', 'slug', 'content', 'user']
     list_display = ('title', 'slug', 'publish_date', 'update_date')
 
 admin.site.register(Document, DocumentAdmin)
