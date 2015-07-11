10429faa264e9867ea307fa44aff8b5040070494
Ahn Jungho <ahnjungho@ahnjungho.org>
2015-07-11 17:03:39 +0900

    add new document list to root page

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,15 @@ from django.http import Http404
 from django.shortcuts import render
 from .models import Document
 
+
 def index(request):
-    documents = Document.objects.order_by('-update_date')[:10]
-    context = {'documents': documents}
+    new_documents = Document.objects.order_by('-publish_date')[:10]
+    recent_documents = Document.objects.order_by('-update_date')[:10]
+
+    context = {'new_documents': new_documents,
+               'recent_documents': recent_documents}
     return render(request, 'pages/index.html', context)
+
 
 def detail(request, slug):
     try:
